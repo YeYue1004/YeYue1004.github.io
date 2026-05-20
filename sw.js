d9193a9e4cef6be0a5190838d6e52730d493066d
SamLin <sam106050102@gmail.com>
2026-05-20 22:29:20 +0800

    Site updated: 2026-05-20 22:29:19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1778242213945";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1779287359163";const preCache = ["/images/taichi.png","/images/banner.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
